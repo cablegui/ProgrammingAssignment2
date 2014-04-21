18840c19d722482913ebb2a006ea67c91718995d
cablegui <neville.andrade@yahoo.com>
2014-04-21 09:15:07 +0200

    Added comments to the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This assignment is used to demonstrate the basic concepts of R. Aspects like functions within functions, list of functions, 
+## <<- operator to assign values to variables which are in an environment above the current environment
 
-## Write a short comment describing this function
-
+## makeCacheMatrix function creates an environment which holds the matrix data supplied in argument x
+## It also holds the basic property functions to interact with the data for setting and getting
 makeCacheMatrix <- function(x = matrix()) 
 {
     
@@ -20,7 +20,9 @@ makeCacheMatrix <- function(x = matrix())
          getInverse = getInverse)
 }
 
-
+## cacheSolve function is a closure function which calculates the inverse of the matrix set in the environment created my makeCacheMatrix function
+## If the function cacheSolve is called on the same function variable then it returns the cached data rather than running solve function again 
+## by checking for null object in the if clause
 cacheSolve <- function(x, ...) 
 {
     m <- x$getInverse()
